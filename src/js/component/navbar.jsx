95eb060575bcd13cea63e725800d98529bb2350b
divRobles <j.roblesmendoza96@gmail.com>
2022-06-10 16:27:16 +0000

    second-commit

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -17,7 +17,7 @@ export const Navbar = () => {
 					<div className="ml-auto">
 
 						<div className="btn-group">
-							<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+							<button type="button" className="btn dropdown-button dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
 								Action
 							</button>
 							<ul className="dropdown-menu">
